64232bf1ad8c11b1d8a9de76f1801e5caa64afd7
Sebastian <sebastian.franz@tum.de>
2026-04-27 15:50:27 +0200

    feat: Adding embedding stats accumulation to AutoEvalReport and EmbeddingStatsTracker

--- biotrainer/embedders/stats/embedding_stats_tracker.py
+++ biotrainer/embedders/stats/embedding_stats_tracker.py
@@ -1,19 +1,43 @@
+from __future__ import annotations
+
 import torch
 import numpy as np
 
-from typing import Iterable
 from pydantic import BaseModel
+from typing import Iterable, Optional
 
 
 class EmbeddingStats(BaseModel):
     embedder_name: str
     dims: int
     n_tracked: int
-    max: float
     min: float
+    max: float
+
+    @staticmethod
+    def from_biotrainer_result(biotrainer_result: dict) -> Optional[EmbeddingStats]:
+        embd_stats = biotrainer_result["derived_values"].get("embedding_stats")
+        if embd_stats is None:
+            return None
+        return EmbeddingStats.model_validate(embd_stats)
+
+    def accumulate_results(self, other: Optional[EmbeddingStats]):
+        if other is None:
+            return self
+        if self.embedder_name != other.embedder_name:
+            raise ValueError(
+                f"Inconsistent embedder name in embedding stats: {self.embedder_name} vs {other.embedder_name}")
+        if self.dims != other.dims:
+            raise ValueError(f"Inconsistent dimensions in embedding stats: {self.dims} vs {other.dims}")
+        self.n_tracked += other.n_tracked
+        self.min = min(self.min, other.min)
+        self.max = max(self.max, other.max)
+        return self
 
 
 class EmbeddingStatsTracker:
+    """ Tracks statistics about embeddings (currently only per-residue embeddings)."""
+
     def __init__(self, embedder_name: str):
         self._embedder_name = embedder_name
 
@@ -34,12 +58,12 @@ class EmbeddingStatsTracker:
         self.n_tracked += per_residue_embedding.shape[0]
         if self.dims is None:
             self.dims = per_residue_embedding.shape[1]
-        self.max = max(self.max, per_residue_embedding.max())
         self.min = min(self.min, per_residue_embedding.min())
+        self.max = max(self.max, per_residue_embedding.max())
 
     def get_stats(self) -> EmbeddingStats:
         return EmbeddingStats(embedder_name=self._embedder_name,
                               dims=self.dims,
                               n_tracked=self.n_tracked,
-                              max=self.max,
-                              min=self.min)
+                              min=self.min,
+                              max=self.max)
