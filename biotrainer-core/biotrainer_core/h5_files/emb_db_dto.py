ebfe8e22f8f7959b242b5c86bfffc44affd83889
Sebastian <sebastian.franz@tum.de>
2026-06-11 12:13:38 +0200

    feat: Adding copy_with to embedding database dto

--- biotrainer-core/biotrainer_core/h5_files/emb_db_dto.py
+++ biotrainer-core/biotrainer_core/h5_files/emb_db_dto.py
@@ -64,3 +64,27 @@ class EmbeddingDatabaseDTO:
             embd_per_residue=per_res_decompressed,
             keep=self.keep,
         )
+
+    def copy_with(
+            self,
+            hash_key: Optional[str] = None,
+            seq_len: Optional[int] = None,
+            access_count: Optional[int] = None,
+            created_at: Optional[datetime] = None,
+            last_accessed: Optional[datetime] = None,
+            embedder_name: Optional[str] = None,
+            embd_per_sequence: Optional = None,
+            embd_per_residue: Optional = None,
+            keep: Optional[bool] = None,
+    ) -> EmbeddingDatabaseDTO:
+        return EmbeddingDatabaseDTO(
+            hash_key=hash_key if hash_key is not None else self.hash_key,
+            seq_len=seq_len if seq_len is not None else self.seq_len,
+            access_count=access_count if access_count is not None else self.access_count,
+            created_at=created_at if created_at is not None else self.created_at,
+            last_accessed=last_accessed if last_accessed is not None else self.last_accessed,
+            embedder_name=embedder_name if embedder_name is not None else self.embedder_name,
+            embd_per_sequence=embd_per_sequence if embd_per_sequence is not None else self.embd_per_sequence,
+            embd_per_residue=embd_per_residue if embd_per_residue is not None else self.embd_per_residue,
+            keep=keep if keep is not None else self.keep,
+        )
