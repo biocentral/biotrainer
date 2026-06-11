a1c7a4a8c769de1a3338a9992a02ff08e636bc36
Sebastian <sebastian.franz@tum.de>
2026-06-11 11:32:24 +0200

    feat: Adding storing functions for embeddings to core

--- biotrainer-core/biotrainer_core/input_files/__init__.py
+++ biotrainer-core/biotrainer_core/input_files/__init__.py
@@ -1,12 +1,14 @@
-from .embeddings import read_id2emb
 from .fasta import read_FASTA, write_FASTA, filter_FASTA
 from .utils import get_split_lists, merge_protein_interactions
+from .embeddings import read_id2emb, store_embedding_to_path, store_embedding_to_handle
 
 __all__ = [
-    "read_id2emb",
     "read_FASTA",
     "write_FASTA",
     "filter_FASTA",
     "get_split_lists",
     "merge_protein_interactions",
+    "read_id2emb",
+    "store_embedding_to_path",
+    "store_embedding_to_handle",
 ]
--- biotrainer-core/biotrainer_core/input_files/embeddings.py
+++ biotrainer-core/biotrainer_core/input_files/embeddings.py
@@ -21,3 +21,16 @@ def read_id2emb(h5_file_path: Union[str, Path], ids_to_load: Optional[Set[str]] 
                 else:
                     raise ValueError(f"Sequence hash {idx} not found in embeddings file!")
     return id2emb
+
+
+def store_embedding_to_path(h5_file_path: Union[str, Path], seq_hash: str, seq_id: str, embedding_data) -> bool:
+    """ Store embedding in h5_file (can be Path or str) """
+    with h5py.File(h5_file_path, "a") as h5_file_handle:
+        store_embedding_to_handle(h5_file_handle, seq_hash, seq_id, embedding_data)
+    return True
+
+def store_embedding_to_handle(h5_file_handle: h5py.File, seq_hash: str, seq_id: str, embedding_data):
+    """ Store embedding in h5_file via h5py.File handle """
+    h5_file_handle.create_dataset(seq_hash, data=embedding_data, compression="gzip", chunks=True)
+    h5_file_handle[seq_hash].attrs["original_id"] = seq_id
+    return True
