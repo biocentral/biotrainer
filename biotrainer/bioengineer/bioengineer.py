fix: Fixing indeterminismn in PGYM bootstrapping calculation
Thanks to Thomas Mauermeier for investigating this!

biotrainer/bioengineer/bioengineer.py
@@ -223,17 +223,18 @@ class BioEngineer:
                 raise ValueError(f"Variant {variant} not found in actual scores!")
 
         # Convert dictionaries to tensors
-        common_variants = set(variant_dict.keys()) & set(actual_scores.keys())
-
         v_d = {m: torch.tensor(v) for m, v in variant_dict.items()}
         a_s = {m: torch.tensor(v) for m, v in actual_scores.items()}
 
+        # Using a dictionary here to avoid python hash indeterminism
+        common_variants = list({k: None for k in (list(v_d.keys()) + list(a_s.keys()))}.keys())
+
         bt_res = Bootstrapper._do_bootstrapping(iterations=30, sample_size=len(common_variants), confidence_level=0.05,
-                                              seq_ids=list(common_variants), all_predictions_dict=v_d,
-                                              all_targets_dict=a_s,
-                                              metrics_calculator=SequenceRegressionMetricsCalculator(device="cpu",
-                                                                                                     n_classes=1)
-                                              )
+                                                seq_ids=common_variants, all_predictions_dict=v_d,
+                                                all_targets_dict=a_s,
+                                                metrics_calculator=SequenceRegressionMetricsCalculator(device="cpu",
+                                                                                                       n_classes=1)
+                                                )
         scc = [res for res in bt_res if res.name == "spearmans-corr-coeff"][0]
         ndcg = [res for res in bt_res if res.name == "ndcg"][0]
         assert scc is not None and ndcg is not None, "Bootstrapping failed!"