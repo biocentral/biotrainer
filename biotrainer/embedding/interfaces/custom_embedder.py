983bdecfb14bb243893a7053e98a6e1d92bb10a8
Sebastian <sebastian.franz@tum.de>
2026-07-09 16:43:05 +0200

    fix: Fixing custom embedder interface

--- biotrainer/embedding/interfaces/custom_embedder.py
+++ biotrainer/embedding/interfaces/custom_embedder.py
@@ -1,13 +1,18 @@
 import torch
-from typing import List, Tuple
+
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Generator
 
 
-class CustomEmbedder:
+class CustomEmbedder(ABC):
     """ Custom Embedder Interface - Used to provide a standardized interface for autoeval custom embedders """
-    def per_residue(self, seqs: List[str]) -> Tuple[str, torch.Tensor]:
-        """ Embed a list of sequences and return a tuple of (sequence, per-residue embedding for this sequence) """
+
+    @abstractmethod
+    def per_residue(self, sequences: List[str]) -> Generator[Tuple[str, torch.Tensor], None, None]:
+        """ Embed a list of sequences and yield a tuple of (sequence, per-residue embedding for this sequence) """
         pass
 
-    def per_sequence(self, seqs: List[str]) -> Tuple[str, torch.Tensor]:
-        """ Embed a list of sequences and return a tuple of (sequence, per-sequence embedding for this sequence) """
+    @abstractmethod
+    def per_sequence(self, sequences: List[str]) -> Generator[Tuple[str, torch.Tensor], None, None]:
+        """ Embed a list of sequences and yield a tuple of (sequence, per-sequence embedding for this sequence) """
         pass
