941c92a08602a83c40bb70a1494acb057f0e480a
Sebastian <sebastian.franz@tum.de>
2026-07-16 14:51:20 +0200

    fix: Setting final datasets for PBC Supervised

--- biotrainer-core/biotrainer_core/data_classes/autoeval/autoeval_pbc_datasets.py
+++ biotrainer-core/biotrainer_core/data_classes/autoeval/autoeval_pbc_datasets.py
@@ -9,10 +9,10 @@ from ..protocol import Protocol
 class PBCSupervisedDatasetName(str, Enum):
     CONSERVATION = "conservation"
     DISORDER_CHEZOD = "disorder_chezod"
-    DISORDER_TRIZOD = "disorder_trizod"
+    # DISORDER_TRIZOD = "disorder_trizod" Currently not used
     FRUSTRATION_CLASSIFICATION = "frustration-classification"
     FRUSTRATION_REGRESSION = "frustration-regression"
-    MEMBRANE = "membrane"
+    # MEMBRANE = "membrane" Currently not used
     PHAGES = "phages"
     SCL = "scl"
     SECONDARY_STRUCTURE = "secondary_structure"
@@ -25,18 +25,12 @@ def all_pbc_supervised_datasets() -> Dict[str, AutoEvalSupervisedDataset]:
                 AutoEvalSupervisedDataset(name=PBCSupervisedDatasetName.DISORDER_CHEZOD,
                                           evaluation_metric="spearmans-corr-coeff",
                                           protocol=Protocol.residue_to_value),
-                AutoEvalSupervisedDataset(name=PBCSupervisedDatasetName.DISORDER_TRIZOD,
-                                          evaluation_metric="spearmans-corr-coeff",
-                                          protocol=Protocol.residue_to_value),
                 AutoEvalSupervisedDataset(name=PBCSupervisedDatasetName.FRUSTRATION_CLASSIFICATION,
                                           evaluation_metric="macro-f1_score",
                                           protocol=Protocol.residue_to_class),
                 AutoEvalSupervisedDataset(name=PBCSupervisedDatasetName.FRUSTRATION_REGRESSION,
                                           evaluation_metric="spearmans-corr-coeff",
                                           protocol=Protocol.residue_to_value),
-                AutoEvalSupervisedDataset(name=PBCSupervisedDatasetName.MEMBRANE,
-                                          evaluation_metric="macro-f1_score",
-                                          protocol=Protocol.residue_to_class),
                 AutoEvalSupervisedDataset(name=PBCSupervisedDatasetName.PHAGES,
                                           evaluation_metric="accuracy",
                                           protocol=Protocol.sequence_to_class),
