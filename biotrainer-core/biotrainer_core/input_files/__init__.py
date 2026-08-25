f37eeb242843d8431fc4b67ec74e3cc9cf210a5e
Sebastian <sebastian.franz@tum.de>
2026-08-25 17:41:47 +0200

    feat: Adding parse_pgym_file (to List of VariantScore) function to core

--- biotrainer-core/biotrainer_core/input_files/__init__.py
+++ biotrainer-core/biotrainer_core/input_files/__init__.py
@@ -1,5 +1,5 @@
 from .npy import load_contact_map
-from .csv import pgym_csv_to_fasta
+from .csv import pgym_csv_to_fasta, parse_pgym_file
 from .fasta import read_FASTA, write_FASTA, filter_FASTA
 from .utils import get_split_lists, merge_protein_interactions
 
@@ -11,4 +11,5 @@ __all__ = [
     "get_split_lists",
     "merge_protein_interactions",
     "pgym_csv_to_fasta",
+    "parse_pgym_file",
 ]
--- biotrainer-core/biotrainer_core/input_files/csv.py
+++ biotrainer-core/biotrainer_core/input_files/csv.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-from typing import Union, Optional
 from pathlib import Path
+from typing import Union, Optional, List
 
 from .fasta import write_FASTA
 
-from ..data_classes import SequenceData
+from ..data_classes import SequenceData, Variant, VariantScore
 
 
 def pgym_csv_to_fasta(pgym_csv_path: Union[str, Path],
@@ -31,3 +31,32 @@ def pgym_csv_to_fasta(pgym_csv_path: Union[str, Path],
                      for seq, (mut, score) in mutation_fitness.items()]
 
     return write_FASTA(output_fasta_path, sequence_data)
+
+
+def parse_pgym_file(pgym_csv_path: Union[str, Path],
+                    single_mutations_only: bool = False) -> List[VariantScore]:
+    # Read ProteinGym dataset
+    df = pd.read_csv(pgym_csv_path)
+    if len(df) == 0:
+        raise ValueError(f"Dataset file {pgym_csv_path} is empty!")
+
+    try:
+        first_row = df.iloc[0]
+        mt_seq = first_row["mutated_sequence"]
+        mutation_string = first_row["mutant"]
+        mutation_fitness = {row["mutant"]: row["DMS_score"] for _, row in df.iterrows()}
+        if single_mutations_only:
+            mutation_fitness = {mut: score for mut, score in mutation_fitness.items() if ":" not in mut}
+    except KeyError as e:
+        raise ValueError(f"Dataset file {pgym_csv_path} is missing a required column: {e}")
+
+    # Derive wild-type sequence
+    one_indexed = True  # ProteinGym default
+    wt_seq = Variant.derive_wildtype_sequence(mutation_sequence=mt_seq, variant_string=mutation_string,
+                                              one_indexed=one_indexed)
+    print(f"Wild-type sequence for {pgym_csv_path} was derived as {wt_seq}.")
+
+    variant_scores = [VariantScore.from_experimental(variant=Variant.parse(variant_string=mutation),
+                                                     experimental_score=score)
+                      for mutation, score in mutation_fitness.items()]
+    return variant_scores
