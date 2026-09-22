bfd49d59e602e844822de725a980e81713e24d10
Sebastian <sebastian.franz@tum.de>
2026-09-22 16:01:53 +0200

    feat: Adding ProtBert as an biotrainer optimized model

--- biotrainer/embedding/huggingface/__init__.py
+++ biotrainer/embedding/huggingface/__init__.py
@@ -1,13 +1,15 @@
 from .ESM2 import ESM2, ESMC
 from .ProtT5 import ProtT5
 from .ProstT5 import ProstT5
+from .ProtBert import ProtBert
 from .huggingface_transformer_embedder import HuggingfaceTransformerEmbedder
 
 OPTIMIZED_EMBEDDERS = [
     ProtT5,
     ProstT5,
+    ProtBert,
     ESM2,
     ESMC,
 ]
 
-__all__ = ["OPTIMIZED_EMBEDDERS", "HuggingfaceTransformerEmbedder", "ProtT5", "ProstT5", "ESM2", "ESMC"]
+__all__ = ["OPTIMIZED_EMBEDDERS", "HuggingfaceTransformerEmbedder", "ProtT5", "ProstT5", "ESM2", "ESMC", "ProtBert"]
